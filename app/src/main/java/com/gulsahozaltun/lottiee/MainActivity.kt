67c59084d05265425b4cbcb4c959ffff357da2b5
gulsahozaltun <64669942+gulsahozaltun@users.noreply.github.com>
2022-02-25 11:53:09 +0300

    Initial commit

---
+++ app/src/main/java/com/gulsahozaltun/lottiee/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.gulsahozaltun.lottiee
+
+import android.graphics.drawable.Animatable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.LayoutInflater
+import android.view.animation.Animation
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContentProviderCompat.requireContext
+import com.airbnb.lottie.Lottie
+import com.airbnb.lottie.LottieDrawable
+import com.gulsahozaltun.lottiee.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        var button=findViewById<Button>(R.id.button)
+
+        button.setOnClickListener{
+
+            //setupAnim()
+            showSuccessDialog()
+        }
+
+
+
+
+
+    }
+
+/*
+
+    private fun setupAnim() {
+        binding.animationView.setAnimation(R.raw.success)
+        binding.animationView.repeatCount = LottieDrawable.INFINITE
+        binding.animationView.playAnimation()
+    }
+*/
+
+
+
+    fun showSuccessDialog() {
+        val layoutView = LayoutInflater.from(this)
+            .inflate(R.layout.success_order, null, false)
+        val dialogBuilder = AlertDialog.Builder(this)
+        dialogBuilder.setView(layoutView)
+        val alertDialog = dialogBuilder.create()
+
+        val timer = object : CountDownTimer(3000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                alertDialog.show()
+                alertDialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+            }
+
+            override fun onFinish() {
+                alertDialog.dismiss()
+                //requireView().findNavController()
+            }
+        }
+        timer.start()
+    }
+}
